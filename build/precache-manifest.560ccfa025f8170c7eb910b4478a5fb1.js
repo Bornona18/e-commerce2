7e94fa8e1e31600dd7b13721f35fcf91ddb46e0b
Bornona18 <61803232+Bornona18@users.noreply.github.com>
2020-09-23 20:32:02 -0400

    Add files via upload

---
+++ build/precache-manifest.560ccfa025f8170c7eb910b4478a5fb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d19b898a29fb7eacb64243dc04a713c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df205a96945d42e41dd2",
+    "url": "/static/css/2.5dddc201.chunk.css"
+  },
+  {
+    "revision": "c40ca3f8f49777b34b5f",
+    "url": "/static/css/main.3b121112.chunk.css"
+  },
+  {
+    "revision": "df205a96945d42e41dd2",
+    "url": "/static/js/2.3069accf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3069accf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40ca3f8f49777b34b5f",
+    "url": "/static/js/main.35e305ce.chunk.js"
+  },
+  {
+    "revision": "a5cd6ca373b6fed798e9",
+    "url": "/static/js/runtime-main.f2d8d710.js"
+  }
+]);
